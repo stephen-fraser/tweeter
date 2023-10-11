16746ab4a8cac85229aad2a284ee89224ba64870
stephen-fraser <steve.james.fraser@gmail.com>
2023-10-11 13:58:01 -0700

    add renderTweets function to client.js to take in an array of objects and dynamically render the tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -29,27 +29,38 @@ $(document).ready(function() {
     $(this).removeClass('highlight-icon');
   })  
 
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-    "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-    "created_at": 1461116232227
-  }
-
   // const $tweet = $(`<article class="tweet">Hello world</article>`);
   // console.log($tweet)
   // $('.tweet-container').append($tweet);
 
-  const createTweetElement = function (tweetData) {
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+  const createTweetElement = function (tweet) {
 
-    const user = tweetData.user;
-    const content = tweetData.content;
+    const user = tweet.user;
+    const content = tweet.content;
 
     let $tweet = `
       <div class="border-tweets">
@@ -64,7 +75,7 @@ $(document).ready(function() {
           <p>${content.text}</p>
         </div>
         <div class="posted-footer">
-          <p>${tweetData.created_at}</p>
+          <p>${tweet.created_at}</p>
           <div>
             <i class="fa-solid fa-flag posted-footer-icons"></i>
             <i class="fa-solid fa-retweet posted-footer-icons"></i>
@@ -76,11 +87,15 @@ $(document).ready(function() {
       return $tweet;
     }
 
-  const $tweet = createTweetElement(tweetData);
+  const renderTweets = function(tweets) {
 
-//   // // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('.tweet-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+    for (let tweet of tweets) {
+      const $tweet = createTweetElement(tweet)
+      $('.tweet-container').append($tweet)
+    }
+  }
+
+  renderTweets(data)
 
 });
 
